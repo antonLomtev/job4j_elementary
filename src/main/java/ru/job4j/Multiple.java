4ea7ef4ee9777cce972cdb8749334a06397b9663
a.stogniy <a.stogniy@astondevs.ru>
2023-11-15 16:19:11 +0300

    Исправлено под checkStyle

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,8 +2,8 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        for (int i = 2; i <= 9; i++){
-            System.out.println(1 + "*" + i + " = " + 1*i);
+        for (int i = 2; i <= 9; i++) {
+            System.out.println(1 + "*" + i + " = " + 1 * i);
         }
     }
 }
